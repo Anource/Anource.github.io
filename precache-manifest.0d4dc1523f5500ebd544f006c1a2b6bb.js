67d2d17b3519610cb26338aa9dc875890f8f7185
Emil Khayrov <hajrov.emil@yandex.ru>
2020-05-18 20:20:39 +0300

    upd

---
+++ precache-manifest.0d4dc1523f5500ebd544f006c1a2b6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd0eacc801ebebcd13ad5d1fe23d8d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc4d59a43e0a6e3cbb95",
+    "url": "/static/css/2.108f3170.chunk.css"
+  },
+  {
+    "revision": "cc4d59a43e0a6e3cbb95",
+    "url": "/static/js/2.18d805d1.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.18d805d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dc64902739850efe5f",
+    "url": "/static/js/main.9474d042.chunk.js"
+  },
+  {
+    "revision": "61323641d3240c3f3a29",
+    "url": "/static/js/runtime-main.b304aab4.js"
+  }
+]);
