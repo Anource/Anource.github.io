7a4221a3c9da0dcbe08646acc0a27312d07812b8
Emil Khayrov <hajrov.emil@yandex.ru>
2020-05-18 21:15:17 +0300

    upd

--- precache-manifest.9f64d8c94d16122daf6c7f54a4d04e99.js
+++ precache-manifest.9f64d8c94d16122daf6c7f54a4d04e99.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f625199b79c53c5745824b76099fc82",
+    "revision": "aecbc5edd584284a7d08a4f5eb890daf",
     "url": "/index.html"
   },
   {
-    "revision": "197edca8fdf637f9613f",
+    "revision": "3df54764a68a7c4df91a",
     "url": "/static/css/2.108f3170.chunk.css"
   },
   {
-    "revision": "197edca8fdf637f9613f",
-    "url": "/static/js/2.83166798.chunk.js"
+    "revision": "3df54764a68a7c4df91a",
+    "url": "/static/js/2.989385dc.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "/static/js/2.83166798.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.989385dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e420f9af764a022e5048",
-    "url": "/static/js/main.e7342d5f.chunk.js"
+    "revision": "959d257fe6975d0ae2e9",
+    "url": "/static/js/main.3249cb5f.chunk.js"
   },
   {
     "revision": "61323641d3240c3f3a29",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91b7be90a6095503958b76326081116a.js"
+  "/precache-manifest.9f64d8c94d16122daf6c7f54a4d04e99.js"
 );
 
 self.addEventListener('message', (event) => {
